b07ed5d6e9dd8ad4d4f680acb8ca2ac362920645
wangjian <wangjianvip83@163.com>
2025-12-22 14:48:48 +0800

    Initial Commit

---
+++ Perapera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Perapera
+//
+//  Created by apple on 2025/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Perapera/PeraperaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeraperaApp.swift
+//  Perapera
+//
+//  Created by apple on 2025/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PeraperaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
